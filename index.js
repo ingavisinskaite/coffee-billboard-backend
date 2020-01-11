76df1e3dbdc8aaf00d5b321707b6130c232e27d8
ingavisinskaite <ingavisinskaite@gmail.com>
2020-01-11 16:56:27 +0200

    integrated with database

--- index.js
+++ index.js
@@ -1,82 +1,144 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const cors = require('cors');
-const coffeeData = require('./coffee-data')
-
-let coffeeList = coffeeData;
+const mysql = require('mysql');
+const databaseCredentials = require('./env');
 
 app.use(bodyParser.json())
 
 const allowedOrigins = ['http://localhost:3000', 'https://ingavisinskaite.github.io'];
 const corsOptions = {
     origin: function (origin, callback) {
-      if (allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
     }
-  }
+}
 
-let port = 3001;
+const port = 3001;
+const coffeeTable = 'coffee_data';
 
 app.options('*', cors(corsOptions))
 app.get('/', (req, res) => {
-    res.json({welcome: "Welcome to Coffee Billboard backend!"})
+    let con = mysql.createConnection(databaseCredentials);
+
+    con.connect(function (err) {
+        if (err) throw err;
+        console.log("Connected!");
+    });
+
 })
 
-app.get('/coffee', cors(corsOptions), (req, res) => res.json({
-    coffeeList
-}))
+app.get('/coffee', cors(corsOptions), (req, res) => {
+    let con = mysql.createConnection(databaseCredentials);
+
+    con.connect(function (err) {
+        if (err) throw err;
+        console.log("Connected!");
+
+        con.query(`SELECT * FROM ${coffeeTable}`, function (err, result, fields) {
+            if (err) throw err;
+            res.json(
+                result
+            );
+        });
+
+        con.end();
+    });
+})
 
 app.post('/coffee', cors(corsOptions), (req, res) => {
-    const imgUrl = req.body.imgUrl;
+    const url = req.body.url;
     const title = req.body.title;
     const price = req.body.price;
-    const id = coffeeList[coffeeList.length - 1].id + 1;
 
-    let newCoffee = {
-        id,
-        imgUrl,
-        title,
-        price,
-    }
+    let con = mysql.createConnection(databaseCredentials);
 
-    coffeeList.push(newCoffee);
+    let query = `INSERT INTO ${coffeeTable} (url, title, price) VALUES ('${url}', '${title}', '${price}');`;
 
-    res.json({
-        newCoffee
-    })
+    con.connect((error) => {
+      console.log(error);
+      con.query(query, (err, result) => {
+        console.log(result);
+        if (err) {
+          res.json(false);
+          throw err;
+        }
+        res.json(result.insertId)
+      });
+  
+      con.end();
+    });
+})
+
+app.get('/coffee/:id', cors(corsOptions), (req, res) => {
+    const id = Number(req.params.id);
+
+    let con = mysql.createConnection(databaseCredentials);
+
+    const query = `SELECT * FROM ${coffeeTable} WHERE id = ` + id;
+
+    con.connect((error) => {
+        console.log(error);
+        con.query(query, (err, result) => {
+          if (err) {
+            res.json(false);
+            throw err;
+          }
+          res.json(result[0]);
+        });
+    
+        con.end();
+      })
 })
 
 app.put('/coffee/:id', cors(corsOptions), (req, res) => {
     const id = Number(req.params.id);
-    const imgUrl = req.body.imgUrl;
+    const url = req.body.url;
     const title = req.body.title;
     const price = req.body.price;
 
-    let editedCoffee = {
-        id,
-        imgUrl,
-        title,
-        price
-    }
+    let con = mysql.createConnection(databaseCredentials);
 
-    let indexInArray = coffeeList.findIndex(coffee => coffee.id === id);
-    coffeeList[indexInArray] = editedCoffee;
+    const query = `UPDATE ${coffeeTable} SET url=?, title=?, price=? WHERE id = ` + id;
 
-    res.json({
-        editedCoffee
-    })
+    con.connect((error) => {
+        console.log(error);
+        con.query(query, [url, title, price], (err, result) => {
+          if (err) {
+            res.json(false);
+            throw err;
+          }
+          console.log(result);
+          res.json(result.insertId);
+          console.log('Successfully edited');
+        });
+    
+        con.end();
+      })
 })
 
 app.delete('/coffee/:id', cors(corsOptions), (req, res) => {
     const id = Number(req.params.id);
+    let con = mysql.createConnection(databaseCredentials);
 
-    coffeeList = coffeeList.filter(coffee => coffee.id !== id);
-
-    res.status(204).send({});
+    con.connect(function (err) {
+        if (err) throw err;
+        console.log('Connected!');
+    
+        con.query(`DELETE FROM ${coffeeTable} WHERE id = ` + id, function (err, result, fields) {
+          if (err) {
+            res.json(false);
+            throw err;
+          }
+          res.json(true);
+        });
+    
+        con.end();
+      })
 })
 
 
